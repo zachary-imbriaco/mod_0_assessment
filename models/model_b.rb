163ad7a6fc4faaacd741bccc1a88ecc135eac32e
Zachary Imbriaco <zachary@imbriaco.com>
2021-12-15 09:51:17 -0500

    create developer model.

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+developer = {
+  first_name: "Zachary",
+  last_name: "Imbriaco",
+  age: 24,
+  favorite_pie: 3.14,
+  knows_javascript: true,
+  knows_ruby: false,
+  affirmation: ["I", "Am", "A", "Developer"],
+  known_languages: ["JavaScript", "Java", "Python"]
+}
