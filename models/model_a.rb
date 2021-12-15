262f067f1500147cb2c65d46262504c5e2134019
Zachary Imbriaco <zachary@imbriaco.com>
2021-12-15 09:47:46 -0500

    create dinosaur model.

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+dinosaur = {
+  name: "T-Rex",
+  scientific_name: "Tyrannosaurus Rex",
+  number_of_legs: 2,
+  number_living: 0,
+  big_head: true,
+  big_arms: false,
+  facial_features: ["eye", "eye", "nostril", "nostril", "mouth"],
+  claws: ["claw", "claw", "claw", "claw", "claw", "claw"]
+}
