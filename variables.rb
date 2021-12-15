d18193c7addbb593f7b24f05398bae7b10fd8324
Zachary Imbriaco <zachary@imbriaco.com>
2021-12-15 09:43:15 -0500

    Complete program specific challenge.

---
+++ variables.rb
@@ -0,0 +1,23 @@
+# assigns string variable movie and boolean variable is_entertaining
+movie = "Star Wars Episode IV: A New Hope"
+is_entertaining = true
+
+# reassigns movie variable
+movie = "Star Wars Episode V: The Empire Strikes Back"
+
+# assigns two integer variables num_4 and num_6
+num_4 = 4
+num_6 = 6
+
+# assigns integer variable product as the product of num_6 and num_4
+product = num_6 * num_4
+
+# assigns array variable favorite_foods
+favorite_foods = ['Pizza', 'Chicken Teriyaki', 'Chicken and Shrimp Carbonara']
+
+# removes first element from favorite_foods array.
+favorite_foods.shift()
+
+# tests outputs
+p favorite_foods
+p movie.length()
